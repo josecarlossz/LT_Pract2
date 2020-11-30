a9c98c8c3c73afcd4ab67833f2a18295e2a22495
josecarlossz <47127293+josecarlossz@users.noreply.github.com>
2020-11-30 12:05:02 +0100

    Create functions.py

---
+++ functions.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+Funciones para calcular los diferentes parámetros del diseno VoIP
+
+@authors: Jose Carlos, Sergio, Manuel, Alejandro
+"""
+
+def retardo_total(Rt,Rr,J,VPS,CSI,Ralg):
+    #Retardo conjunto, CODEC + paquetizacion
+    Rorg = VPS + 0.1*CSI
+    
+    #Retardos buffer antijitter
+    Rjitter_1 = 1.5*J
+    Rjitter_2 = 2*J
+    
+    #Retardo de decodificacion en el destino
+    Rdest = 0.1*VPS
+
+    #Retardo total (buffer antijitter x1.5 y x2)
+    Rt1 = Rorg + Ralg + Rr + Rjitter_1 + Rdest
+    Rt2 = Rorg + Ralg + Rr + Rjitter_2 + Rdest
+    
+    #Calculamos el numero de paquetes almacenados en el buffer antijitter
+    #en cada caso
+    Npaq1 = Rjitter_1//VPS
+    Npaq2 = Rjitter_2//VPS
+    
+    return Rorg, Ralg, Rr, Rjitter_1, Rjitter_2, Rdest, Rt1, Rt2, Npaq1, Npaq2
+
+
+    
+    
+    
